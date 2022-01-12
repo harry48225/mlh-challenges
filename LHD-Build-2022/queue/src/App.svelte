29da887288133ce964df9aa66f8bb55b4fd32247
harry48225 <harrybest800@gmail.com>
2022-01-12 20:37:42 +0000

    Add falling box

--- LHD-Build-2022/queue/src/App.svelte
+++ LHD-Build-2022/queue/src/App.svelte
@@ -1,30 +1,49 @@
 <script lang="ts">
-	export let name: string;
+  import { Bodies, Composite, Runner } from 'matter-js';
+  import { Render } from 'matter-js';
+  import { Engine } from 'matter-js';
+import { onMount } from 'svelte';
+  
+  onMount(() => {
+    const engine = Engine.create();
+    const render = Render.create({
+      canvas: document.getElementById("canvas") as HTMLCanvasElement,
+      engine: engine
+    });
+
+    const ground = Bodies.rectangle(400, 610, 810, 70, {isStatic: true, restitution: 1});
+    const box = Bodies.rectangle(400, 100, 50, 50, {restitution: 1});
+    Composite.add(engine.world, [ground, box]);
+
+    Render.run(render);
+
+    const runner = Runner.create()
+
+    Runner.run(runner, engine);
+  })
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  
+  <main>
+    <h1>Queue</h1>
+    <canvas id="canvas"></canvas>
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      padding: 1em;
+      max-width: 240px;
+      margin: 0 auto;
+    }
+  
+    canvas {
+      height: auto;
+      width: 80%;
+    }
+  
+    @media (min-width: 640px) {
+      main {
+        max-width: none;
+      }
+    }
+  </style>
